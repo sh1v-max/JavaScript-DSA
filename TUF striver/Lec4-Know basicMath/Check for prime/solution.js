605bbf1c842dadd3e1c5483ba53379df06f0844f
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-05-31 20:51:05 +0530

    question 3 striver solved

--- TUF striver/Lec4-Know basicMath/Check for prime/solution.js
+++ TUF striver/Lec4-Know basicMath/Check for prime/solution.js
@@ -1,5 +1,13 @@
 class Solution {
-  countDigit(n) {
-      return n.toString().length
+  isPrime(n) {
+      //your code goes here
+      if (n <= 1) return false;
+
+      for (let i = 2; i < n; i++) {
+          if (n % i === 0) {
+              return false;
+          }
+      }
+      return true;
   }
 }
