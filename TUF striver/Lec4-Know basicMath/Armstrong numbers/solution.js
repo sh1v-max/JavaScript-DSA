ce08a80239ad56bb894a4ac6effb1348b189c6e7
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-05-31 20:05:42 +0530

    question 1 striver solved

--- TUF striver/Lec4-Know basicMath/Armstrong numbers/solution.js
+++ TUF striver/Lec4-Know basicMath/Armstrong numbers/solution.js
@@ -1,5 +1,9 @@
 class Solution {
-  countDigit(n) {
-      return n.toString().length
+  isArmstrong(n) {
+      const digits = n.toString().split('').map((n) => Number(n));
+      // console.log(digits)
+      const num = digits.length;
+      const sum = digits.reduce((acc, digit) => acc + Math.pow(digit, num), 0);
+      return sum === n;
   }
 }
