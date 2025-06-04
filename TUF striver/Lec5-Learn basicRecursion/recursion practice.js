d405a215914f5179ffa5abefb03d249812595693
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-06-04 20:41:00 +0530

    recursion practice

--- TUF striver/Lec5-Learn basicRecursion/recursion practice.js
+++ TUF striver/Lec5-Learn basicRecursion/recursion practice.js
@@ -167,3 +167,18 @@ function revArr5(arr, left = 0, right = arr.length - 1) {
 const arr = [1, 2, 3, 4, 5]
 console.log(revArr5(arr))
 // console.log(arr)
+
+//==============================================================================
+// ? fibonacci series
+
+function fib(n) {
+  if (n === 0) {
+    return 0
+  }
+  if (n === 1) {
+    return 1
+  }
+  return fib(n - 1) + fib(n - 2)
+}
+
+// console.log(fib(5))
