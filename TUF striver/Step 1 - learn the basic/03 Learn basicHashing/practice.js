be721f4f940139b7c07bba7402a0d62afc3bc9a6
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-06-06 15:20:28 +0530

    practicing hash

---
+++ TUF striver/Step 1 - learn the basic/03 Learn basicHashing/practice.js
@@ -0,0 +1,12 @@
+// this is how we take input from user
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("Enter your name: ", function(name) {
+  console.log("Hello, " + name);
+  rl.close();
+});
