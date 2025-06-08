6f08f36547fd6a79cd338d58a3773cd34d3edc2e
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-06-08 20:48:28 +0530

    gcd of string leetcode

---
+++ LeetCode/1071-Greatest-common-divisor-of-string/1071-Greatest-common-divisor-of-string.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} str1
+ * @param {string} str2
+ * @return {string}
+ */
+var gcdOfStrings = function (str1, str2) {
+  if (str1 + str2 !== str2 + str1) {
+      return "";
+  }
+
+  const lenGCD = gcd(str1.length, str2.length);
+  
+  return str1.slice(0, lenGCD);
+};
+
+function gcd(a, b) {
+  while (b !== 0) {
+      [a, b] = [b, a % b];
+  }
+  return a;
+}
