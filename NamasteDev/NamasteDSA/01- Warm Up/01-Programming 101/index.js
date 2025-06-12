47b88ea36f5fabe5fc011dc3232b9223e0575981
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-06-12 20:48:35 +0530

    if else and function done

--- NamasteDev/NamasteDSA/01- Warm Up/01-Programming 101/index.js
+++ NamasteDev/NamasteDSA/01- Warm Up/01-Programming 101/index.js
@@ -30,3 +30,17 @@ console.log(h+i)
 // this will concatenate the string h with the number x and print the result.
 // Since x is a number, it will be converted to a string and concatenated with h.
 // The result will be "Namaste30"
+
+let arr=[1,2,3,4,5]
+console.log(arr)
+console.log(arr[0])
+let j = arr[0]+arr[1]
+console.log(j)
+arr.push(6)
+console.log(arr)
+arr.pop()
+console.log(arr)
+arr.unshift(0)
+console.log(arr)
+arr.shift()
+console.log(arr)
