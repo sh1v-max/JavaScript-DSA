3b1fef1c5c754dc8b07e3738292d771b3b719a8b
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-06-12 19:21:15 +0530

    basic js

---
+++ NamasteDev/NamasteDSA/01- Warm Up/01-Programming 101/index.js
@@ -0,0 +1,29 @@
+console.log("Hello")
+let a=10
+let b=5
+let c= a+b
+console.log(c)
+
+let d=a*b
+console.log(d)
+
+let e=a/b
+console.log(e)
+
+let f=a%b
+console.log(f)
+
+let g=a**b
+console.log(g)
+
+let h="Namaste"
+console.log(h)
+
+let i = "DSA"
+console.log(i)
+console.log(h+i)
+
+let x = 20
+console.log(x)
+x = 30
+console.log(x)
