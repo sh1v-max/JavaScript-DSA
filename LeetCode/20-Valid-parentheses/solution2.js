dae408175bc103a722af39be5a92a9e4a36742db
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-06-14 22:15:24 +0530

    valid parentheses

---
+++ LeetCode/20-Valid-parentheses/solution2.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+  while (s.includes("()") || s.includes("{}") || s.includes("[]")) {
+          s = s.replace("()", "");
+          s = s.replace("{}", "");
+          s = s.replace("[]", "");
+      }
+      return s === "";
+  
+};
