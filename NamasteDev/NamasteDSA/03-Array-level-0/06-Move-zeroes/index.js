911c93db29983dd9d9d156c9ac789af6f64d2584
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-06-20 19:06:51 +0530

    move zeroes solved

--- NamasteDev/NamasteDSA/03-Array-level-0/06-Move-zeroes/index.js
+++ NamasteDev/NamasteDSA/03-Array-level-0/06-Move-zeroes/index.js
@@ -1,14 +1,14 @@
 // move all zeroes to the end of the array, using in-place method
 
 function moveZeroes(nums) {
-  let count = 0;
+  let x = 0;
   for (let i = 0; i < nums.length; i++) {
     if (nums[i] !== 0) {
-      nums[count] = nums[i];
-      count++;
+      nums[x] = nums[i];
+      x++;
     }
   }
-  for (let i = count; i < nums.length; i++) {
+  for (let i = x; i < nums.length; i++) {
     nums[i] = 0;
   }
 }
@@ -16,3 +16,26 @@ function moveZeroes(nums) {
 const nums = [0, 1, 0, 3, 12];
 moveZeroes(nums);
 console.log(nums);
+
+// another approach, without in-place method
+function moveZeroes2(nums) {
+  let result = [];
+
+  // Step 1: Push non-zero elements
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      result.push(nums[i]);
+    }
+  }
+
+  // Step 2: Add the same number of zeros at the end
+  let zeroCount = nums.length - result.length;
+  for (let i = 0; i < zeroCount; i++) {
+    result.push(0);
+  }
+
+  return result;
+}
+const nums2 = [0, 1, 0, 3, 12];
+moveZeroes2(nums2);
+console.log(nums2);
