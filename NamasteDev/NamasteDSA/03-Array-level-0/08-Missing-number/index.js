9351f053526749da5dd02d6752abb917d3e2192e
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-06-21 19:25:01 +0530

    using xor

--- NamasteDev/NamasteDSA/03-Array-level-0/08-Missing-number/index.js
+++ NamasteDev/NamasteDSA/03-Array-level-0/08-Missing-number/index.js
@@ -56,14 +56,14 @@ const nums = [9,6,4,2,3,5,7,0,1];
 
 var missingNumber2 = function(nums) {
   let n = nums.length;
-  let total_sum = (n * (n + 1)) / 2;
-  let sum_of_array = 0;
+  let totalSum = (n * (n + 1)) / 2;
+  let sum = 0;
 
   for (let num of nums) {
-      sum_of_array += num;
+      sum += num;
   }
 
-  return total_sum - sum_of_array;
+  return totalSum - sum;
 };
 
 const nums2 = [9,6,4,2,3,5,7,0,1];
