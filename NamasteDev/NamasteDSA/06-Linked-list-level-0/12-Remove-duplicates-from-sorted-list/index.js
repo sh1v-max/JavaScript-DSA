0850995bedad14e06b7d5ed65c021e5ca0ab2602
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-07-20 18:56:29 +0530

    revision linked list

--- NamasteDev/NamasteDSA/06-Linked-list-level-0/12-Remove-duplicates-from-sorted-list/index.js
+++ NamasteDev/NamasteDSA/06-Linked-list-level-0/12-Remove-duplicates-from-sorted-list/index.js
@@ -2,10 +2,7 @@
 // return head
 
 // approach:
-// iterate through the list with curr pointer
-// check if curr.val === curr.next.val
-// if yes, skip the node by curr.next = curr.next.next
-// else, curr = curr.next
+
 
 function removeDuplicates(head) {
   let curr = head;
