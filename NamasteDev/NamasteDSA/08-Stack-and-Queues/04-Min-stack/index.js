7d071c0832f8bb264820f443ea8526ceea2e2245
sh1v-max <singhshiv0427@gmail.com>
2025-08-23 19:06:19 +0530

    revision

--- NamasteDev/NamasteDSA/08-Stack-and-Queues/04-Min-stack/index.js
+++ NamasteDev/NamasteDSA/08-Stack-and-Queues/04-Min-stack/index.js
@@ -2,7 +2,13 @@
 // and retrieving the minimum element in constant time
 // time complexity must be O(1) for each operation
 
-
+// Approach:
+// Use a stack of pairs [val, minForThatVal]
+// On push: store [val, min(val, currentMin)]
+// we will store data as array (val and min)
+// On pop: remove the top
+// top(): return top[0]
+// getMin(): return top[1]
 
 var MinStack = function () {
   this.s = []
